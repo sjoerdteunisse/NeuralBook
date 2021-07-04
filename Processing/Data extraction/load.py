a8d48471c4703650ec6c3e4ee90be1cbddb960d0
Sjoerd Teunisse <sjoerdteunisse@gmail.com>
2021-07-04 21:33:51 +0200

    data load

---
+++ Processing/Data extraction/load.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+def load_data_csv(path):
+    print('loading;', path)
+
+    data = pd.read_csv(path)
+
+    print('data', data.shape)
+
+    print('nUni', df.nunique())
+    print('nNull', df.isnull().sum())
+    return data
+
