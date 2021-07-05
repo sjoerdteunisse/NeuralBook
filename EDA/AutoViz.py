68956246afe993bf02b04e1e02232f02950ec9a8
Sjoerd Teunisse <sjoerdteunisse@gmail.com>
2021-07-05 21:41:58 +0200

    fix

--- EDA/AutoViz.py
+++ EDA/AutoViz.py
@@ -2,5 +2,5 @@ import pandas as pd
 from autoviz.AutoViz_Class import AutoViz_Class
 AV = AutoViz_Class()
 
-def AutoViz(frame):
+def AutoViz(frame, sep, target, df):
     dft = AV.AutoViz(frame, sep=sep, depVar=target, dfte=df, header=0, verbose=2, lowess=False,chart_format='svg',max_rows_analyzed=1500,max_cols_analyzed=30)
